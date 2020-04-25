86a9e2c755027d51cbda5deaf6b8f67f7d35d148
tintinthong <justinthong93@gmail.com>
2020-04-25 23:09:58 +0800

    Adding infix operator

--- src/Card.elm
+++ src/Card.elm
@@ -18,11 +18,12 @@ import Element exposing (
     paddingXY, 
     alignLeft,
     image)
-
 import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font
 import Element.Input as Input
+import Tuple 
+
 
 
 main = 
@@ -34,27 +35,34 @@ type alias Model = { content : String}
 init : Model 
 init = { content = "" }
 
-type alias CardData = { title: String , subtitle: String}
+type alias CardData = { title: String , subtitle: String, imageUrl: String}
 
 newCard : CardData
-newCard = {title =  "Some title" , subtitle = ""} 
+newCard = {title =  "Some title" , subtitle = "", imageUrl = Tuple.second imageURLs} 
 
+isEven : Int -> Bool
+isEven = (\x -> modBy 2 0 == 0)
+
+chooseImage : Int -> String 
+chooseImage n = if(isEven n) then Tuple.second imageURLs else Tuple.first imageURLs
+
+imageURLs = (  "https://bit.ly/2VS0QBW","https://bit.ly/3eMl8FA")
 
 generateCards : Int -> List CardData 
 generateCards n = 
     case n of
     0 -> []
-    _ -> List.append [ { title = String.fromInt n , subtitle = "Subtitle" }] <| generateCards (n-1) 
+    _ -> [ { title = String.fromInt n , subtitle = "Subtitle", imageUrl = chooseImage n }] ++ generateCards (n-1) 
     --_ -> let x = generateCards (n-1) in 
     --    List.append [ { title = String.fromInt n , subtitle = "hehe" }] x
-    
 transformListUI : List CardData -> List (Element msg)
-transformListUI xs = List.map card xs  
+transformListUI xs = List.map card xs     
+
  
 rowOfCards =
     row [ width fill, centerY, spacing 10,padding 10, Border.width 2 ] 
         <| transformListUI 
-        <| generateCards 2 
+        <| generateCards 3 
 
 type Msg = Maybe String  
 
@@ -82,24 +90,6 @@ myButton =
         , label = text "My Button"
         }
 
-myCard : CardData -> Element msg
-myCard cardData =
-    el
-        [ Background.color (rgb255 255 255 255)
-        , Font.color (rgb255 0 0 0)
-        , Border.color (rgb255 0 0 0)
-        , Border.width 5 
-        , padding 30
-        , Font.family
-            [ Font.external
-                { name = "Montserrat"
-                , url = "https://fonts.googleapis.com/css?family=Montserrat"
-                }
-            , Font.sansSerif
-           ]
-        ]
-        (Element.text <| cardData.title ++ cardData.subtitle )
-        
         
 card : CardData -> Element msg
 card cardData = Element.el
@@ -114,7 +104,7 @@ card cardData = Element.el
         Element.column [spacing 10]
         [ 
             headerUI {title = cardData.title, subtitle= cardData.subtitle}
-            ,imageUI
+            ,imageUI cardData.imageUrl
             ,descriptionUI "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
             
         ]
@@ -175,10 +165,9 @@ descriptionUI content = Element.paragraph
             Element.text <| content 
             ]
 
-imageUI : Element msg 
-imageUI = image [Border.width 2, width fill] 
-            { src = "https://bit.ly/3eMl8FA" , description = "music-reader screenshot"}
-            
+imageUI : String -> Element msg 
+imageUI url = image [Border.width 2, width fill] 
+            { src = url, description = "Some image"}
  
  
  
